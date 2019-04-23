e02d016bdd5715d67097c8ecdae9b1cf3032826a
Malik Norford <malik.norford@gmail.com>
2019-04-23 16:20:36 -0400

    this is the update to date so far

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { ModeratorComponent } from './components/pages/moderator/moderator.compo
 import { PreferencesComponent } from './components/pages/preferences/preferences.component';
 import { NavbarComponent } from './components/layout/navbar/navbar.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { ServicesComponent } from './services/services.component';
+// import { DirectivesComponent } from './directives/directives.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import { FooterComponent } from './components/layout/footer/footer.component';
     ModeratorComponent,
     PreferencesComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    LoginComponent,
+    ServicesComponent,
+    // DirectivesComponent
   ],
   imports: [
     BrowserModule,
